de5b00424c49efe0514eaac2c92bf4baf4d9cda3
foxcsong <76721750+foxcsong@users.noreply.github.com>
2025-12-23 15:23:56 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // 这行代码非常关键：它把 Cloudflare 后台设置的环境变量注入到前端代码中
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    'process.env.AUTH_USERS': JSON.stringify(process.env.AUTH_USERS),
   },
   build: {
     outDir: 'dist',
